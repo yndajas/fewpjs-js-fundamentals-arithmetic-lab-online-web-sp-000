d4b49c578c616f5ea81873e27df2a1e8f15e31bb
Ynda Jas <yndajas@gmail.com>
2021-03-21 23:22:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 4;
+const num2 = 15.5;
+
+const multiply = num1 * num2;
+
+const random = Math.random() + 0.000000001;
+
+const num3 = 24;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(0, 1, 20);
